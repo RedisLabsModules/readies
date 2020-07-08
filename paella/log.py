de065754cbd1c4a1f8603489fae6ff3a4881836e
rafie <rafi@redislabs.com>
2020-07-08 14:50:52 +0300

    Fixes in getredis, getdocker

--- paella/log.py
+++ paella/log.py
@@ -1,6 +1,16 @@
 
 import os
+import inspect
 
+def caller_info(back_frames=1):
+    back_frames += 1
+    frame = inspect.currentframe()
+    while back_frames > 0:
+        frame = frame.f_back
+        back_frames -= 1
+    (filename, line_number, function_name, lines, index) = inspect.getframeinfo(frame)
+    return "%s:%d %s" % (filename, line_number, function_name)
+    
 def fatal(text):
-	eprint("%s: %s" % (os.path.basename(__file__), text))
+	eprint("%s: %s" % (caller_info(1), text))
 	exit(1)
