7e5505d26d7a55f7047ee68a451807c96d6413c1
rafie <rafi@redislabs.com>
2021-12-08 11:23:11 +0200

    paella/sh: fixed strip() problem

--- paella/utils3.py
+++ paella/utils3.py
@@ -12,12 +12,12 @@ def sh(cmd, join=False, lines=False):
     out, err = proc.communicate()
     if proc.returncode != 0:
         raise RuntimeError(err.decode('utf-8'))
-    out = out.decode('utf-8')
+    out = out.decode('utf-8').strip()
     if lines is True:
         join = False
     if lines is True or join is not False:
         out = out.split("\n")
     if join is not False:
         s = join if type(join) is str else ' '
-        out = s.join(out).strip()
+        out = s.join(out)
     return out
