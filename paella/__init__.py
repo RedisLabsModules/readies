00214db0b66a6a23cb60bae2fbf3efde5b27ef41
rafie <rafi@redislabs.com>
2022-05-29 20:00:38 +0300

    Python2-related fix (global_injector)

--- paella/__init__.py
+++ paella/__init__.py
@@ -18,11 +18,11 @@ from .contrib.version import Version
 class global_injector:
     def __init__(self):
         try:
-            # Python 3
-            self.__dict__['builtin'] = sys.modules['builtins'].__dict__
-        except KeyError:
             # Python 2
             self.__dict__['builtin'] = sys.modules['__builtin__'].__dict__
+        except KeyError:
+            # Python 3
+            self.__dict__['builtin'] = sys.modules['builtins'].__dict__
     def __setattr__(self,name,value):
         self.builtin[name] = value
 
